9d8d57dac141fa35f92cfbf9953db2b1617daa45
jae-ux <jehmalwright5@gmail.com>
2025-04-15 23:53:33 -0400

    v4

--- sketch.js
+++ sketch.js
@@ -1,23 +1,19 @@
 
 let fireworks = [];
 let gravity;
-let fireworkSound;
-let fft;
 let started = false;
+let whistleSound, explosionSound;
 
 function preload() {
   soundFormats('mp3');
-  fireworkSound = loadSound('fiery-whistle-firework-missile-explodes-in-the-night-sky-258872.mp3');
+  whistleSound = loadSound('firework-whistle.mp3');
+  explosionSound = loadSound('firework-explosion.mp3');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB);
-  gravity = createVector(0, 0.2);
-  textFont('Courier New');
-  textSize(36);
-  textAlign(CENTER, CENTER);
-  fft = new p5.FFT();
+  gravity = createVector(0, 0.13);
   background(0);
 }
 
@@ -25,14 +21,18 @@ function draw() {
   if (!started) {
     background(0);
     fill(255);
+    textSize(28);
+    textAlign(CENTER, CENTER);
     text('Tap anywhere to start the show 🎇', width / 2, height / 2);
     return;
   }
 
-  background(0, 0, 0, 25);
+  colorMode(RGB);
+  background(0, 0, 0, 30);
 
-  if (random(1) < 0.02) {
+  if (random(1) < 0.025) {
     fireworks.push(new Firework());
+    whistleSound.play();
   }
 
   for (let i = fireworks.length - 1; i >= 0; i--) {
@@ -44,21 +44,25 @@ function draw() {
   }
 
   fill(255);
-  text("Hi Sarah, I like you 💖", width / 2, height - 50);
+  textSize(32);
+  textAlign(CENTER);
+  text('Hi Sarah, I like you 💖', width / 2, height - 50);
 }
 
 function touchStarted() {
-  if (!started) {
-    userStartAudio();
-    started = true;
-    fireworkSound.loop();
-  }
+  started = true;
+  userStartAudio();
+  return false;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 class Firework {
   constructor() {
-    this.hu = random(255);
-    this.firework = new Particle(random(width), height, this.hu, true);
+    this.hu = random(360);
+    this.firework = new Particle(random(width * 0.2, width * 0.8), height, this.hu, true);
     this.exploded = false;
     this.particles = [];
   }
@@ -71,9 +75,10 @@ class Firework {
     if (!this.exploded) {
       this.firework.applyForce(gravity);
       this.firework.update();
-      if (this.firework.vel.y >= 0) {
+      if (this.firework.vel.y >= random(-2, 0)) {
         this.exploded = true;
         this.explode();
+        explosionSound.play();
       }
     }
 
@@ -87,41 +92,36 @@ class Firework {
   }
 
   explode() {
-    let spectrum = fft.analyze();
-    let energy = fft.getEnergy("bass");
-    let count = map(energy, 0, 255, 30, 120);
-
-    for (let i = 0; i < count; i++) {
-      const p = new Particle(this.firework.pos.x, this.firework.pos.y, this.hu, false);
+    let num = int(random(70, 130));
+    for (let i = 0; i < num; i++) {
+      const angle = map(i, 0, num, 0, TWO_PI);
+      const mag = random(3, 7);
+      const vel = p5.Vector.fromAngle(angle).mult(mag);
+      const p = new Particle(this.firework.pos.x, this.firework.pos.y, this.hu + random(-15, 15), false, vel);
       this.particles.push(p);
     }
   }
 
   show() {
-    if (!this.exploded) {
-      this.firework.show();
-    }
-
-    for (let p of this.particles) {
-      p.show();
-    }
+    if (!this.exploded) this.firework.show();
+    for (let p of this.particles) p.show();
   }
 }
 
 class Particle {
-  constructor(x, y, hu, firework) {
+  constructor(x, y, hu, firework, vel = null) {
     this.pos = createVector(x, y);
     this.prevPos = this.pos.copy();
+    this.hu = hu;
     this.firework = firework;
     this.lifespan = 255;
-    this.hu = hu;
     this.acc = createVector(0, 0);
 
-    if (this.firework) {
-      this.vel = createVector(0, random(-6, -3)); // slower ascent
+    if (firework) {
+      this.vel = createVector(0, random(-13, -10));
     } else {
-      this.vel = p5.Vector.random2D();
-      this.vel.mult(random(2, 10));
+      this.vel = vel || p5.Vector.random2D().mult(random(2, 6));
+      this.drag = random(0.91, 0.95);
     }
   }
 
@@ -131,31 +131,29 @@ class Particle {
 
   update() {
     this.prevPos = this.pos.copy();
-
     if (!this.firework) {
-      this.vel.mult(0.9);
-      this.lifespan -= 4;
+      this.vel.mult(this.drag);
+      this.lifespan -= map(this.vel.mag(), 0, 5, 0.5, 1.5);
     }
-
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.mult(0);
   }
 
   done() {
-    return this.lifespan < 0;
+    return this.lifespan < 10;
   }
 
   show() {
     colorMode(HSB);
-    strokeWeight(2);
-    let glow = color(this.hu, 255, 255, this.lifespan);
-    stroke(glow);
+    strokeWeight(this.firework ? 2 : 1);
+    stroke(this.hu, 100, 255, this.lifespan);
     line(this.prevPos.x, this.prevPos.y, this.pos.x, this.pos.y);
 
-    // Soft bloom glow
-    noStroke();
-    fill(this.hu, 255, 255, this.lifespan / 4);
-    ellipse(this.pos.x, this.pos.y, 12);
+    if (!this.firework) {
+      noStroke();
+      fill(this.hu, 100, 255, this.lifespan / 5);
+      ellipse(this.pos.x, this.pos.y, 4);
+    }
   }
 }
